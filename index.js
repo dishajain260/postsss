d34cbabd6b08f838f3ab3a100e18db13d0e4392b
Disha Jain <dishajain260@gmail.com>
2024-03-08 17:30:16 +0530

    df

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const methodOverride=require("method-override");
 
 app.use(express.urlencoded({extended:true}));
+
 app.use(methodOverride('_method'));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -54,7 +55,7 @@ app.patch("/posts/:id",(req,res)=>{
     let post=posts.find((p) => id=== p.id);
     post.content=newcontent;
     console.log(post);
-    res.send("patch request working");
+    res.redirect("/posts");
 });
 app.get("/posts/:id/edit",(req,res)=>{
     
@@ -63,6 +64,14 @@ app.get("/posts/:id/edit",(req,res)=>{
     let post=posts.find((p) => id=== p.id);
    
     res.render("edit.ejs",{post});
+});
+app.delete("/posts/:id",(req,res)=>{
+    
+    let {id}=req.params;
+    
+     posts=posts.filter((p) => id!== p.id);
+   
+     res.redirect("/posts");
 });
 app.listen(port,()=>{
     console.log("listening to port:8080");
